eb02cbc8dbe43948352978c7360d130d43075cdc
Wei Jie <jackchong398@gmail.com>
2023-10-08 23:07:51 +0800

    feat: scan tcp ports

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/Cwjiee/port-scanner/port"
 )
 
 func main() {
 
 	fmt.Println("port scanning")
-	
-
+	results := port.ScanAllPorts("localhost")
+	fmt.Println(results)
 }
--- port/port.go
+++ port/port.go
@@ -1,8 +1,41 @@
-package port 
+package port
+
+import (
+	"net"
+	"time"
+	"strconv"
+)
 
 type ScanResult struct {
 	Port int 
 	Status string
+}
+
+func ScanPort(protocol, hostname string, port int) ScanResult {
+	
+	result := ScanResult{Port: port}
+	address := hostname + ":" + strconv.Itoa(port)
+	conn, err := net.DialTimeout(protocol, address, 60*time.Second)
+
+	if err != nil {
+		result.Status = "Closed"
+		return result
+	}
+
+	conn.Close()
+	result.Status = "Open"
+	return result
+}
+
+func ScanAllPorts(hostname string) []ScanResult {
+	
+	var results []ScanResult
+
+	for i := 0; i <= 1024; i++ {
+		results = append(results, ScanPort("tcp", hostname, i))
+	}
+
+	return results
 }
 
 
